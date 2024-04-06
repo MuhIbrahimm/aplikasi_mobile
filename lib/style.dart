ad85a53eb1a746ba80036ea3ef45cda84c2a6312
MuhIbrahimm <baim21121994@gmail.com>
2024-04-06 09:49:17 +0800

    add style.dart

---
+++ lib/style.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const TextStyle baseTextStyle = TextStyle(
+  fontFamily: 'Poppins', // Replace with your desired font family
+);
+
+// Define heading styles
+const TextStyle h1Style = TextStyle(
+  fontSize: 32.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.lightBlue,
+);
+
+const TextStyle h2Style = TextStyle(
+  fontSize: 24.0,
+  fontWeight: FontWeight.w500,
+  color: Colors.lightBlue,
+);
+
+// Define text style
+const TextStyle bodyTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: Colors.lightBlue,
+);
